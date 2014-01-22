e55137f7d0fefc26ff8fb9c6d99da8ca27bb92fe
Deep Datta Roy <deepdattaroy8888@gmail.com>
2014-01-21 20:22:08 -0500

    Got something that compiles

--- gtthread.h
+++ gtthread.h
@@ -2,55 +2,114 @@
 #define __GTTHREAD_H
 
 #include <stdlib.h>
-#include "gtthread_sched.h"
 #include "gtthread_mutex.h"
+#include "gtthread_sched.h"
 
-typedef struct
-{
-    uint32_t flags;
-    void * stack_base;
-    size_t stack_size;
-    size_t guard_size;
-    int32_t sched_policy;
-    int32_t sched_priority;
-} gtthread_attr_t;
+#define MEM 64000
 
-typedef unsigned int gtthread_t;
+//Thread id structure
+typedef unsigned int gtthread_t;	
 
-int RRPeriod = 0;
-
-
-/* Must be called before any of the below functions. Failure to do so may
- * result in undefined behavior. 'period' is the scheduling quantum (interval)
- * in microseconds (i.e., 1/1000000 sec.). */
+//////////////////////////////////
+//gtthread_init()
+//
+//parameters: 
+//      long period - the RR period
+//returns: 
+//      none
+//
+//Initialize threading library
+//////////////////////////////////
 void gtthread_init(long period);
 
-/* see man pthread_create(3); the attr parameter is omitted, and this should
- * behave as if attr was NULL (i.e., default attributes) */
+//////////////////////////////////
+//gtthread_create()
+//
+//parameters: 
+//      gtthread_t *thread - thread id
+//      void *(*start_routine)(void *) - routine
+//      void *arg - arguments
+//returns: 
+//      int - id
+//
+//Initialize threading library
+//////////////////////////////////
 int  gtthread_create(gtthread_t *thread,
                      void *(*start_routine)(void *),
                      void *arg);
 
-/* see man pthread_join(3) */
+//////////////////////////////////
+//gtthread_join()
+//
+//parameters: 
+//      gtthread_t thread - thread id
+//		void **status - 
+//returns: 
+//      int - success
+//
+//Join thread with given id
+//////////////////////////////////
 int  gtthread_join(gtthread_t thread, void **status);
 
-/* gtthread_detach() does not need to be implemented; all threads should be
- * joinable */
-
-/* see man pthread_exit(3) */
+//////////////////////////////////
+//gtthread_exit()
+//
+//parameters: 
+//      void *retval - the return val
+//returns: 
+//      none
+//
+//Exit thread
+//////////////////////////////////
 void gtthread_exit(void *retval);
 
-/* see man sched_yield(2) */
-int gtthread_yield(void);
+//////////////////////////////////
+//gtthread_yield()
+//
+//parameters: 
+//      none
+//returns: 
+//      none
+//
+//Change context to next thread
+//////////////////////////////////
+int gtthread_yield();
 
-/* see man pthread_equal(3) */
+//////////////////////////////////
+//gtthread_equal()
+//
+//parameters: 
+//      gtthread_t t1 - first thread
+//		gtthread_t t2 - second thread
+//returns: 
+//      none
+//
+//Change context to next thread
+//////////////////////////////////
 int  gtthread_equal(gtthread_t t1, gtthread_t t2);
 
-/* see man pthread_cancel(3); but deferred cancelation does not need to be
- * implemented; all threads are canceled immediately */
+//////////////////////////////////
+//gtthread_cancel()
+//
+//parameters: 
+//      gtthread_t thread
+//returns: 
+//      int - success
+//
+//Kill thread with given id
+//////////////////////////////////
 int  gtthread_cancel(gtthread_t thread);
 
-/* see man pthread_self(3) */
-gtthread_t gtthread_self(void);
+//////////////////////////////////
+//gtthread_self()
+//
+//parameters: 
+//      none
+//returns: 
+//      gtthread_t - id of thread
+//
+//Change context to next thread
+//////////////////////////////////
+gtthread_t gtthread_self();
 
 #endif // __GTTHREAD_H
